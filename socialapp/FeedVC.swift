278b759e42e869e43cc2e6e48c4364be13feaa8b
Hannan Saleemi <hannansaleemi@Hannans-Mac.local>
2017-01-07 16:48:27 +0000

    Parse Firebase Data

--- socialapp/FeedVC.swift
+++ socialapp/FeedVC.swift
@@ -14,6 +14,8 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,9 +25,26 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         //Listerners - listens for changes eg) likes/posts removed etc
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in          //Refrence to database - posts section
             
-            print(snapshot.value!)                                          //This prints JSON data of posts in the database and updates in realtime automatically
+            //print(snapshot.value!)                          //This prints JSON data of posts in the database and updates in realtime automatically
+            
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot]{       //Get eveyrthing under posts
+                
+                for snap in snapshot{
+                    //print("SNAP: \(snap)")                      //PRINT objects with the post data inside eg) likes, imageURL, caption
+                    
+                    if let postDict = snap.value as? Dictionary<String, AnyObject>{     //Parse data
+                        let key = snap.key                                              //Get ket is possible
+                        let post = Post(postKey: key, postData: postDict)               //Get post key and value pairs
+                        self.posts.append(post)                                         //Append to post array
+                    }
+                    
+                }
+                
+            }
+            self.tableView.reloadData()                          //Once we have the posts reload table view with all posts
             
         })
+        
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {       //Sections mostly one
@@ -33,17 +52,21 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {       //How many cells
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print("Hannan: \(post.caption)")
+        
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
 
     
     @IBAction func signOutTapped(_ sender: UITapGestureRecognizer) {
         
-        KeychainWrapper.standard.removeObject(forKey: KEY_UID)      //Remove key from KEYCHAIN fro autologin
+        KeychainWrapper.standard.removeObject(forKey: KEY_UID)      //Remove key from KEYCHAIN from autologin
         try! FIRAuth.auth()?.signOut()                              //Sign out of Firebase
         performSegue(withIdentifier: "goToSignIn", sender: nil)     //Back to Login Screen
         
---
+++ socialapp/Post.swift
@@ -0,0 +1,57 @@
+//
+//  Post.swift
+//  socialapp
+//
+//  Created by Hannan Saleemi on 07/01/2017.
+//  Copyright © 2017 Hannan Saleemi. All rights reserved.
+//
+
+import Foundation
+
+class Post{
+    
+    private var _caption: String!
+    private var _imageUrl: String!
+    private var _likes: Int!
+    private var _postKey: String!
+    
+    var caption: String{
+        return _caption
+    }
+    
+    var imageUrl: String{
+        return _imageUrl
+    }
+    
+    var likes: Int{
+        return _likes
+    }
+    
+    var postKey: String{
+        return _postKey
+    }
+    
+    init(caption: String, imageUrl: String, likes: Int) {
+        self._caption = caption
+        self._imageUrl = imageUrl
+        self._likes = likes
+    }
+    
+    init(postKey: String, postData: Dictionary<String, AnyObject>){        //Parse data we recive from firebase
+        self._postKey = postKey
+        
+        if let caption = postData["caption"] as? String{                          //get caption from JSON DATA
+            self._caption = caption
+        }
+        
+        if let imageUrl = postData["imageUrl"] as? String{                       //get imageurl from JSON DATA
+            self._imageUrl = imageUrl
+        }
+        
+        if let likes = postData["likes"] as? Int{                               //get likes from JSON DATA
+            self._likes = likes
+        }
+        
+    }
+    
+}
